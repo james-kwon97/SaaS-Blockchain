753bceaeb3a1df64bb9044723fa26327a4dd8d10
James Kwon <jhk1097@gmail.com>
2024-12-06 18:16:53 +1300

    Duplicating the hamburger menu and setting them apart for two lines

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -8,8 +8,11 @@ export const HeaderSection = () => {
           </div>
           <div>
             <div className="size-10 relative">
-              <div className="absolute">
-                <div className="w-5 h-0.5 bg-zinc-300"></div>
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
+              </div>
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
               </div>
             </div>
           </div>
