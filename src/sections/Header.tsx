890be749ef5f7a3a5a85309561992eb811ba6ca0
James Kwon <jhk1097@gmail.com>
2024-12-06 18:14:38 +1300

    Adding our blog logo and hamburger menu

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,8 +3,16 @@ export const HeaderSection = () => {
     <div>
       <div className="container">
         <div className="flex justify-between">
-          <div></div>
-          <div></div>
+          <div>
+            <img src="/assets/images/logo.svg" alt="Blockforge logo" />
+          </div>
+          <div>
+            <div className="size-10 relative">
+              <div className="absolute">
+                <div className="w-5 h-0.5 bg-zinc-300"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
