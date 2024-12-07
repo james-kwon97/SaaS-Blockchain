507e5d68b0eb0756681f7f56d2ea2b3ff8b392fd
James Kwon <jhk1097@gmail.com>
2024-12-07 21:26:41 +1300

    Adding a button and styling it

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,7 +6,10 @@ export const HeaderSection = () => {
           <div>
             <img src="/assets/images/logo.svg" alt="Blockforge logo" />
           </div>
-          <div>
+          <div className="flex gap-4 items-center">
+            <button className="bg-fuchsia-500/20 px-4 py-2 font-extrabold uppercase font-heading text-sm tracking-wide">
+              Get Started
+            </button>
             <div className="size-10 relative">
               <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
                 <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
