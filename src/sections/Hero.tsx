25757315e1fc58f24bbec2f0f5f80d19cb542ec7
James Kwon <jhk1097@gmail.com>
2024-12-08 22:38:33 +1300

    Creating HeroSection and filling in basic contents

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,11 @@
+export const HeroSection = () => {
+  return (
+    <section>
+      <div className="container">
+        <p>Introducing Blockforge</p>
+        <h1>The Future of Blockchain is here.</h1>
+        <p>Blockforge is pioneering smart contract integrity with cutting-edge data solutions.</p>
+      </div>
+    </section>
+  );
+};
