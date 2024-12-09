3634009b4d05149be6ec8f8bca4f1f4f197ff5dd
James Kwon <jhk1097@gmail.com>
2024-12-09 17:56:50 +1300

    Importing Button and also the Icosahedron image

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,5 @@
+import { CutCornerButton } from '../components/CutCornerButton';
+
 export const HeroSection = () => {
   return (
     <section>
@@ -5,6 +7,8 @@ export const HeroSection = () => {
         <p>Introducing Blockforge</p>
         <h1>The Future of Blockchain is here.</h1>
         <p>Blockforge is pioneering smart contract integrity with cutting-edge data solutions.</p>
+        <CutCornerButton className="">Get Started</CutCornerButton>
+        <img src="/assets/images/icosahedron.png" alt="Icosahedron 3D Image" />
       </div>
     </section>
   );
