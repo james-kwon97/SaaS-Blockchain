d3cd71d02ce3c10e50c2eae724f5dfa913108261
James Kwon <jhk1097@gmail.com>
2024-12-09 17:50:38 +1300

    Adding props to our button

--- src/components/CutCornerButton.tsx
+++ src/components/CutCornerButton.tsx
@@ -1,6 +1,9 @@
-export const CutCornerButton = () => {
+import type { ComponentPropsWithoutRef } from 'react';
+
+export const CutCornerButton = (props: ComponentPropsWithoutRef<'button'>) => {
+  const { className, children } = props;
   return (
-    <button className="bg-fuchsia-500/20 px-4 py-2 font-extrabold uppercase font-heading text-sm tracking-wide relative">
+    <button className="bg-fuchsia-500/20 px-4 py-2 font-extrabold uppercase font-heading text-sm tracking-wide relative hidden md:inline-flex">
       <div className="absolute inset-0 outline outline-2 -outline-offset-2 outline-fuchsia-500 [mask-image:linear-gradient(225deg,transparent,transparent_10px,black_10px)]"></div>
       <svg
         width="24"
@@ -12,7 +15,7 @@ export const CutCornerButton = () => {
       >
         <path d="M0 1H12.2667L23 11.7333V24" stroke="currentColor" stroke-width="2"></path>
       </svg>
-      <span className="leading-6">Get Started</span>
+      <span className="leading-6">{children}</span>
     </button>
   );
 };
