9de05f49e4800f68c862f0e2e370536b8ef3b711
James Kwon <jhk1097@gmail.com>
2024-12-09 17:52:25 +1300

    Using twMerge to add classes on top of our base stylings of the button

--- src/components/CutCornerButton.tsx
+++ src/components/CutCornerButton.tsx
@@ -1,9 +1,15 @@
 import type { ComponentPropsWithoutRef } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 export const CutCornerButton = (props: ComponentPropsWithoutRef<'button'>) => {
   const { className, children } = props;
   return (
-    <button className="bg-fuchsia-500/20 px-4 py-2 font-extrabold uppercase font-heading text-sm tracking-wide relative hidden md:inline-flex">
+    <button
+      className={twMerge(
+        'bg-fuchsia-500/20 px-4 py-2 font-extrabold uppercase font-heading text-sm tracking-wide relative',
+        className,
+      )}
+    >
       <div className="absolute inset-0 outline outline-2 -outline-offset-2 outline-fuchsia-500 [mask-image:linear-gradient(225deg,transparent,transparent_10px,black_10px)]"></div>
       <svg
         width="24"
