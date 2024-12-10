5384ec2515039efda183bf80ccd786e864f6311b
James Kwon <jhk1097@gmail.com>
2024-12-10 19:41:59 +1300

    Adjusting desktop view screen's fonts and image sizes

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,7 +7,7 @@ export const HeroSection = () => {
         <p className="uppercase font-extrabold text-center text-zinc-500 tracking-wide">
           Introducing Blockforge
         </p>
-        <h1 className="font-heading font-black text-5xl md:text-6xl text-center mt-4">
+        <h1 className="font-heading font-black text-5xl md:text-6xl lg:text-7xl text-center mt-4 max-w-3xl mx-auto">
           The Future of Blockchain is here.
         </h1>
         <p className="text-center text-xl md:text-2xl mt-6 text-zinc-400 max-w-xl mx-auto">
@@ -16,13 +16,19 @@ export const HeroSection = () => {
         <div className="flex justify-center mt-10">
           <CutCornerButton className="">Get Started</CutCornerButton>
         </div>
-        <div className="inline-flex mt-24 relative z-0">
-          <img
-            src="/assets/images/icosahedron.png"
-            alt=""
-            className="absolute w-[calc(100%+100px)] max-w-none -z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 saturate-[10%] brightness-[4%] hue-rotate-[240deg]"
-          />
-          <img src="/assets/images/icosahedron.png" alt="Icosahedron 3D Image" />
+        <div className="flex justify-center mt-24 ">
+          <div className="inline-flex relative z-0">
+            <img
+              src="/assets/images/icosahedron.png"
+              alt=""
+              className="absolute w-[calc(100%+100px)] max-w-none -z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 saturate-[10%] brightness-[4%] hue-rotate-[240deg]"
+            />
+            <img
+              src="/assets/images/icosahedron.png"
+              alt="Icosahedron 3D Image"
+              className="w-[500px]"
+            />
+          </div>
         </div>
       </div>
     </section>
