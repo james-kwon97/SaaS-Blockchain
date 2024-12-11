6fb14a3ce0d62021a9f53e0850beaeff3f7ec90a
James Kwon <jhk1097@gmail.com>
2024-12-11 20:08:46 +1300

    Adding extra hexagons and also positioning a 3d cube on top

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -22,6 +22,14 @@ export const HeroSection = () => {
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <Hexagon className="size-[1100px]" />
             </div>
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+              <Hexagon className="size-[1800px]" />
+            </div>
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+              <div className="bg-zinc-900 size-[240px] inline-flex items-center justify-center rounded-full outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10 absolute left-[200px] -top-[900px]">
+                <img src="/assets/images/cube.png" alt="Cube 3D image" className="size-[140px]" />
+              </div>
+            </div>
             <img
               src="/assets/images/icosahedron.png"
               alt=""
