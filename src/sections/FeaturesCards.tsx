3a0dd7cc2df51a9c6ccd6e5a775fc79c419911b4
James Kwon <jhk1097@gmail.com>
2024-12-13 23:14:08 +1300

    Forming my card section contents

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -1,3 +1,21 @@
 export const FeaturesCardsSection = () => {
-  return <h1>hi there</h1>;
+  return (
+    <section>
+      <div className="container">
+        <h2>Discover the future of blockchain with Blockforge</h2>
+        <div>
+          <div>
+            <h3>Revolutionary Blockchain API</h3>
+            <p>
+              Effortlessly integrate and manage blockchain data with our cutting-edge API, designed
+              for seamless connectivity.
+            </p>
+            <div className="flex">
+              <button>Learn More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
