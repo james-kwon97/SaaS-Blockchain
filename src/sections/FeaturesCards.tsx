41ea9f79505be143916bd37abfe31ddfedfb8525
James Kwon <jhk1097@gmail.com>
2024-12-13 23:16:31 +1300

    Importing arrow icon from heroicon and pill image

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -5,6 +5,7 @@ export const FeaturesCardsSection = () => {
         <h2>Discover the future of blockchain with Blockforge</h2>
         <div>
           <div>
+            <img src="/assets/images/pill.png" alt="Pill image" />
             <h3>Revolutionary Blockchain API</h3>
             <p>
               Effortlessly integrate and manage blockchain data with our cutting-edge API, designed
@@ -12,6 +13,20 @@ export const FeaturesCardsSection = () => {
             </p>
             <div className="flex">
               <button>Learn More</button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                />
+              </svg>
             </div>
           </div>
         </div>
