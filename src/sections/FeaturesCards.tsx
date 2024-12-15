1cc048073395fd21629fb4b5ad986feefabd5277
James Kwon <jhk1097@gmail.com>
2024-12-15 22:02:11 +1300

    Making our arrow icon's colour slightly darker

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -27,7 +27,7 @@ export const FeaturesCardsSection = () => {
                 viewBox="0 0 24 24"
                 strokeWidth={2}
                 stroke="currentColor"
-                className="size-8"
+                className="size-8 text-zinc-500"
               >
                 <path
                   strokeLinecap="round"
