f56121c03bdfc028513f8e2105d964377b355f41
James Kwon <jhk1097@gmail.com>
2024-12-16 22:36:52 +1300

    Adding effect to shadow when pill image animates

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -11,7 +11,7 @@ export const FeaturesCardsSection = () => {
             <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
             <div className="flex justify-center -mt-28">
               <div className="inline-flex relative">
-                <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]"></div>
+                <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]"></div>
                 <img
                   src="/assets/images/pill.png"
                   alt="Pill image"
