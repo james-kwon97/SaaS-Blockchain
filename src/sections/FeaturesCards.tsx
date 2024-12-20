914963643560d56ffd71f53a43385bfe0ba8bbc6
James Kwon <jhk1097@gmail.com>
2024-12-20 22:01:34 +1300

    Adding other conditions of different coloured cards

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -46,12 +46,16 @@ export const FeaturesCardsSection = () => {
                   className={twMerge(
                     'absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 blur-lg opacity-0 group-hover:opacity-100 transition duration-300',
                     color === 'lime' && 'bg-lime-500',
+                    color === 'cyan' && 'bg-cyan-500',
+                    color === 'violet' && 'bg-violet-500',
                   )}
                 ></div>
                 <div
                   className={twMerge(
                     'absolute size-16 rounded-xl bg-fuchsia-500 group-hover:bg-fuchsia-400 transition duration-300 top-1.5 right-1.5 -z-10',
                     color === 'lime' && 'bg-lime-500 group-hover:bg-lime-400',
+                    color === 'cyan' && 'bg-cyan-500 group-hover:bg-cyan-400',
+                    color === 'violet' && 'bg-violet-500 group-hover:bg-violet-400',
                   )}
                 ></div>
                 <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
@@ -72,6 +76,8 @@ export const FeaturesCardsSection = () => {
                     className={twMerge(
                       'text-sm font-heading uppercase font-extrabold tracking-wider text-fuchsia-500',
                       color === 'lime' && 'text-lime-500',
+                      color === 'cyan' && 'text-cyan-500',
+                      color === 'violet' && 'text-violet-500',
                     )}
                   >
                     Learn More
