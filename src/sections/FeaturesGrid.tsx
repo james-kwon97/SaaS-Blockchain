202af361b610df8a456fad0effeb52a4fd25ae68
James Kwon <jhk1097@gmail.com>
2024-12-21 18:22:38 +1300

    Adding contents into our grid.tsx

--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -1,3 +1,26 @@
+const listItems = [
+  'Experience unparalleled security and scalability',
+  'Fully benefit from scalable network effects',
+  'Unlock the potential of decentralised networks',
+];
+
 export const FeaturesGrid = () => {
-  return <div>features</div>;
+  return (
+    <section>
+      <div className="container">
+        <h2>Empowering the future of blockchain.</h2>
+        <p>
+          Blockforge provides robust and secure infrastructure to support the next generation of
+          decentralised applications.
+        </p>
+        <ul>
+          {listItems.map((item) => (
+            <li key={item}>
+              <span>{item}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
