97ea5cddc3220ffa9e3eb636def43b354a2ff1f2
James Kwon <jhk1097@gmail.com>
2024-12-21 18:37:27 +1300

    Styling the h2 and p for our mobile views

--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -9,23 +9,25 @@ const listItems = [
 
 export const FeaturesGrid = () => {
   return (
-    <section>
+    <section py-24>
       <div className="container">
-        <h2>Empowering the future of blockchain.</h2>
-        <p>
-          Blockforge provides robust and secure infrastructure to support the next generation of
-          decentralised applications.
-        </p>
-        <ul>
-          {listItems.map((item) => (
-            <li key={item}>
-              <span>{item}</span>
-            </li>
-          ))}
-        </ul>
         <div>
-          <CutCornerButton>Get Started</CutCornerButton>
-          <TextButton>Learn More</TextButton>
+          <h2 className="font-heading font-black text-4xl">Empowering the future of blockchain.</h2>
+          <p className="text-xl text-zinc-400 mt-8">
+            Blockforge provides robust and secure infrastructure to support the next generation of
+            decentralised applications.
+          </p>
+          <ul>
+            {listItems.map((item) => (
+              <li key={item}>
+                <span>{item}</span>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <CutCornerButton>Get Started</CutCornerButton>
+            <TextButton>Learn More</TextButton>
+          </div>
         </div>
       </div>
     </section>
