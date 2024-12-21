4981aad9ed1500bb45ff3a0772e4b8f3b9c3bab7
James Kwon <jhk1097@gmail.com>
2024-12-21 18:41:06 +1300

    Adding and styling bullet points next to our texts

--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -17,10 +17,13 @@ export const FeaturesGrid = () => {
             Blockforge provides robust and secure infrastructure to support the next generation of
             decentralised applications.
           </p>
-          <ul>
+          <ul className="flex flex-col gap-8 mt-12">
             {listItems.map((item) => (
-              <li key={item}>
-                <span>{item}</span>
+              <li key={item} className="flex items-center gap-3">
+                <div className="inline-flex flex-shrink-0 justify-center items-center size-8 outline outline-4 -outline-offset-4 outline-fuchsia-500/10 rounded-full">
+                  <div className="size-1.5 bg-fuchsia-500 rounded-full"></div>
+                </div>
+                <span className="text-xl font-bold">{item}</span>
               </li>
             ))}
           </ul>
