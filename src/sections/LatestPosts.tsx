a8286cee9e79282490d43f2741f06bafb19f5a00
James Kwon <jhk1097@gmail.com>
2024-12-25 19:24:08 +1300

    Adding our post contents

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -1,3 +1,27 @@
 export const LatestPosts = () => {
-  return <section>Latest posts</section>;
+  return (
+    <section>
+      <div className="container">
+        <h2>Your portal to everything blockchain.</h2>
+        <p>
+          Keep up with the newest trends, updates, and insights in the blockchain world, updated
+          weekly.
+        </p>
+        <div>
+          <div>
+            <div>Technology</div>
+            <h3>Regulatory Challenges Facing Blockchain</h3>
+            <p>
+              Understanding the regulatory landscape surrounding blockchain and what it means for
+              the future of this technology.
+            </p>
+            <div>
+              <button>Read more</button>
+              <div>arrow</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
