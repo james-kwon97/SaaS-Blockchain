4edc4dce614c29f9d84d36ddc422f75af8b11905
James Kwon <jhk1097@gmail.com>
2024-12-28 22:56:55 +1300

    Replacing our dummy array with our latestPosts props

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -14,8 +14,8 @@ export const LatestPosts = (props: { latestPosts: CollectionEntry<'blog'>[] }) =
           weekly.
         </p>
         <div className="mt-16 flex flex-col gap-8">
-          {[...new Array(4)].fill(0).map((item, itemIndex) => (
-            <Card key={itemIndex} buttonText="Read More">
+          {latestPosts.map((post, postIndex) => (
+            <Card key={postIndex} buttonText="Read More">
               <div className="px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-fuchsia-500/15 text-fuchsia-500 inline-flex rounded-full">
                 Technology
               </div>
