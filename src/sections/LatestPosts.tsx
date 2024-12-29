91d6c19c5ff9fbde57a15952b1b06ac8e1c13954
James Kwon <jhk1097@gmail.com>
2024-12-29 22:48:10 +1300

    Replacing our static description with the data description

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -14,16 +14,13 @@ export const LatestPosts = (props: { latestPosts: CollectionEntry<'blog'>[] }) =
           weekly.
         </p>
         <div className="mt-16 flex flex-col gap-8">
-          {latestPosts.map(({ data: { title } }, postIndex) => (
+          {latestPosts.map(({ data: { title, description } }, postIndex) => (
             <Card key={postIndex} buttonText="Read More">
               <div className="px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-fuchsia-500/15 text-fuchsia-500 inline-flex rounded-full">
                 Technology
               </div>
               <h3 className="font-heading font-black text-3xl mt-4">{title}</h3>
-              <p className="text-lg text-zinc-400 mt-6">
-                Understanding the regulatory landscape surrounding blockchain and what it means for
-                the future of this technology.
-              </p>
+              <p className="text-lg text-zinc-400 mt-6">{description}</p>
             </Card>
           ))}
         </div>
