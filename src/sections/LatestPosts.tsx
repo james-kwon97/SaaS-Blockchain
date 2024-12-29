9d5cdcfb5587228fc890c1016704109a423525d1
James Kwon <jhk1097@gmail.com>
2024-12-29 22:46:29 +1300

    Replacing the hard coded title with our data titles

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -14,14 +14,12 @@ export const LatestPosts = (props: { latestPosts: CollectionEntry<'blog'>[] }) =
           weekly.
         </p>
         <div className="mt-16 flex flex-col gap-8">
-          {latestPosts.map((post, postIndex) => (
+          {latestPosts.map(({ data: { title } }, postIndex) => (
             <Card key={postIndex} buttonText="Read More">
               <div className="px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-fuchsia-500/15 text-fuchsia-500 inline-flex rounded-full">
                 Technology
               </div>
-              <h3 className="font-heading font-black text-3xl mt-4">
-                Regulatory Challenges Facing Blockchain
-              </h3>
+              <h3 className="font-heading font-black text-3xl mt-4">{title}</h3>
               <p className="text-lg text-zinc-400 mt-6">
                 Understanding the regulatory landscape surrounding blockchain and what it means for
                 the future of this technology.
