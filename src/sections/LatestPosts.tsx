0fc58e8060c61d66c0a4e092dcc0b7fe222e301c
James Kwon <jhk1097@gmail.com>
2024-12-29 22:48:42 +1300

    Adding our data category to our posts

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -14,10 +14,10 @@ export const LatestPosts = (props: { latestPosts: CollectionEntry<'blog'>[] }) =
           weekly.
         </p>
         <div className="mt-16 flex flex-col gap-8">
-          {latestPosts.map(({ data: { title, description } }, postIndex) => (
+          {latestPosts.map(({ data: { title, description, category } }, postIndex) => (
             <Card key={postIndex} buttonText="Read More">
               <div className="px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-fuchsia-500/15 text-fuchsia-500 inline-flex rounded-full">
-                Technology
+                {category}
               </div>
               <h3 className="font-heading font-black text-3xl mt-4">{title}</h3>
               <p className="text-lg text-zinc-400 mt-6">{description}</p>
