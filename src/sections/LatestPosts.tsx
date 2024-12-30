0a16909d78f1e082e029fe58a18bbed0bac22476
James Kwon <jhk1097@gmail.com>
2024-12-30 23:42:29 +1300

    Adding grid instead of flex to make mobile view 1 column, where as tablet views will have 2 columns

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -9,14 +9,14 @@ export const LatestPosts = (props: { latestPosts: CollectionEntry<'blog'>[] }) =
   return (
     <section className="py-60">
       <div className="container">
-        <h2 className="font-heading font-black text-4xl text-center">
+        <h2 className="font-heading font-black text-4xl md:text-5xl text-center">
           Your portal to everything blockchain.
         </h2>
         <p className="text-xl text-center text-zinc-400 mt-8">
           Keep up with the newest trends, updates, and insights in the blockchain world, updated
           weekly.
         </p>
-        <div className="mt-16 flex flex-col gap-8">
+        <div className="mt-16 grid grid-cols-1 md:grid-cols-2 gap-8">
           {latestPosts.map(({ data: { title, description, category } }, postIndex) => (
             <Card key={postIndex} buttonText="Read More" color={getPostColorFromCategory(category)}>
               <Tag color={getPostColorFromCategory(category)}>{category}</Tag>
