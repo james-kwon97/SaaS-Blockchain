80512f1939cfc9c1db462acf70d2e080a7f1e65e
James Kwon <jhk1097@gmail.com>
2025-01-01 23:39:01 +1300

    Adding quotation marks using Html and styling testimonial's name and job descriptions

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -5,14 +5,14 @@ export const TestimonialsSection = () => {
         <div>
           {[...new Array(3)].fill(0).map((_,index) => (<div>
             <blockquote>
-              <p className="font-heading text-3xl font-black">The user experience is phenomenal, and the support team is always there to help. Highly recommended!</p>
-              <cite>
+              <p className="font-heading text-3xl font-black">&ldquo;The user experience is phenomenal, and the support team is always there to help. Highly recommended!&rdquo;</p>
+              <cite className="mt-8 block">
                 <div>
                   <div>
-                    <div>avatar</div>
+                    <div className="size-16 bg-zinc-700 rounded-full"></div>
                   </div>
-                  <div>Erica Wyatt</div>
-                  <div>Product Manager - BlockLink</div>
+                  <div className="text-lg not-italic font-black ">Erica Wyatt</div>
+                  <div className="text-zinc-400 not-italic">Product Manager - BlockLink</div>
                 </div>
               </cite>
             </blockquote>
