9a370b7f843105f1f733576ec2c71c19a48857c0
James Kwon <jhk1097@gmail.com>
2025-01-01 23:18:22 +1300

    Adding our testimonials content and repeating using a dummy array

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,3 +1,21 @@
 export const TestimonialsSection = () => {
-  return <section>testimonials</section>
+  return <section>
+    <div>
+      {[...new Array(3)].fill(0).map((_,index) => (<div>
+        <blockquote>
+          <p>The user experience is phenomenal, and the support team is always there to help. Highly recommended!</p>
+          <cite>
+            <div>
+              <div>
+                <div>avatar</div>
+              </div>
+              <div>Erica Wyatt</div>
+              <div>Product Manager - BlockLink</div>
+            </div>
+
+          </cite>
+        </blockquote>
+      </div>))}
+    </div>
+  </section>
 }
