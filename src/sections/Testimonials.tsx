7e5c76d5604f3a135b20b48dda1b66e0b48a8623
James Kwon <jhk1097@gmail.com>
2025-01-01 23:35:45 +1300

    Adding a container, padding and adjusting text sizes

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,21 +1,24 @@
 export const TestimonialsSection = () => {
-  return <section>
-    <div>
-      {[...new Array(3)].fill(0).map((_,index) => (<div>
-        <blockquote>
-          <p>The user experience is phenomenal, and the support team is always there to help. Highly recommended!</p>
-          <cite>
-            <div>
-              <div>
-                <div>avatar</div>
-              </div>
-              <div>Erica Wyatt</div>
-              <div>Product Manager - BlockLink</div>
-            </div>
-
-          </cite>
-        </blockquote>
-      </div>))}
-    </div>
-  </section>
+  return (
+    <section className="py-32 bg-zinc-800">
+      <div className="container">
+        <div>
+          {[...new Array(3)].fill(0).map((_,index) => (<div>
+            <blockquote>
+              <p className="font-heading text-3xl font-black">The user experience is phenomenal, and the support team is always there to help. Highly recommended!</p>
+              <cite>
+                <div>
+                  <div>
+                    <div>avatar</div>
+                  </div>
+                  <div>Erica Wyatt</div>
+                  <div>Product Manager - BlockLink</div>
+                </div>
+              </cite>
+            </blockquote>
+          </div>))}
+        </div>
+      </div>
+    </section>
+  )
 }
