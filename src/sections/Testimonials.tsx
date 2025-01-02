97743cae581c695e05c22e3c5566f7ee0ba405e9
James Kwon <jhk1097@gmail.com>
2025-01-02 20:16:36 +1300

    Adding grid and adding flex to blockquote

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -2,12 +2,12 @@ export const TestimonialsSection = () => {
   return (
     <section className="py-32 bg-zinc-800">
       <div className="container">
-        <div>
+        <div className="grid grid-cols-1 gap-16">
           {[...new Array(3)].fill(0).map((_,index) => (<div>
             <blockquote>
               <p className="font-heading text-3xl font-black">&ldquo;The user experience is phenomenal, and the support team is always there to help. Highly recommended!&rdquo;</p>
               <cite className="mt-8 block">
-                <div>
+                <div className="flex gap-3 items-center">
                   <div>
                     <div className="size-16 bg-zinc-700 rounded-full"></div>
                   </div>
