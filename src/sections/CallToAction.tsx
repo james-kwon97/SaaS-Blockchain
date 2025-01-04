78cbf855c77b83955c04d4a93261738df6ca5237
James Kwon <jhk1097@gmail.com>
2025-01-04 20:14:36 +1300

    Adding titles and texts

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,11 @@
+import { CutCornerButton } from "../components/CutCornerButton"
+
 export const CallToAction = () => {
-  return <section>call to action</section>
+  return <section>
+    <div className="container">
+      <h2>Ready to get started?</h2>
+      <p>Start building using blockchain technology, with Blockforge.</p>
+      <CutCornerButton>Get Started</CutCornerButton>
+    </div>
+  </section>
 }
