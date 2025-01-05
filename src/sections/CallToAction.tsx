2631fc14d386ed746486e5c971d78bd691fe1fd3
James Kwon <jhk1097@gmail.com>
2025-01-05 22:29:34 +1300

    Aligning everything to fit my tablet views

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -3,8 +3,8 @@ import { CutCornerButton } from "../components/CutCornerButton"
 export const CallToAction = () => {
   return <section className="py-60">
     <div className="container">
-      <h2 className="font-heading font-black text-4xl text-center">Ready to <span className="block"></span>get started?</h2>
-      <p className="text-xl text-zinc-400 text-center mt-8">Start building using blockchain technology, with Blockforge.</p>
+      <h2 className="font-heading font-black text-4xl md:text-5xl text-center">Ready to <span className="block"></span>get started?</h2>
+      <p className="text-xl text-zinc-400 text-center mt-8 max-w-sm mx-auto">Start building using blockchain technology, with Blockforge.</p>
       <div className="flex justify-center mt-12">
         <CutCornerButton>Get Started</CutCornerButton>
       </div>
