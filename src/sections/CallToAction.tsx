72491d09e6177e7b7e36addbdd11dba0c18295e1
James Kwon <jhk1097@gmail.com>
2025-01-06 19:35:38 +1300

    Adding cylinder image to outer hexagon design

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -17,7 +17,11 @@ export const CallToAction = () => {
             <img src="/assets/images/cuboid.png" className="size-[140px]"/>
           </Circle>
         </div>
-        
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Circle className="absolute -left-[600px] -top-[70px]">
+            <img src="/assets/images/cylinder.png" className="size-[140px]"/>
+          </Circle>
+        </div>
         <h2 className="font-heading font-black text-4xl md:text-5xl lg:text-6xl text-center">Ready to <span className="block"></span>get started?</h2>
         <p className="text-xl lg:text-2xl text-zinc-400 text-center mt-8 max-w-sm mx-auto">Start building using blockchain technology, with Blockforge.</p>
         <div className="flex justify-center mt-12">
