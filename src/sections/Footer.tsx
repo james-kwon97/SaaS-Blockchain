4f96f5ed01f3e91dbc6c04ff82ceae18dd4d861c
James Kwon <jhk1097@gmail.com>
2025-01-07 22:50:20 +1300

    Adding our contents into footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,25 @@
 export const Footer = () => {
-  return <footer>Footer</footer>
+  return <footer>
+    <div className="container">
+      <div>
+        <div>
+          <img src="/assets/images/logo.svg" alt="Blockforge logo"/>
+        </div>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Blog</a>
+          <a href="#">Careers</a>
+          <a href="#">Contact</a>
+        </nav>
+
+      </div>
+      <div>
+        <p>&copy; 2025 Blockforge. All rights reserved.</p>
+        <div>
+          <div>discord</div>
+        </div>
+
+      </div>
+    </div>
+  </footer>
 }
