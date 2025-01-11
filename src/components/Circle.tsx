b5770f2e9da1368e691bbc7a423831638bde2104
James Kwon <jhk1097@gmail.com>
2025-01-11 21:32:46 +1300

    Splitting our border's classes so that we get a hint of colour on the top border

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -6,10 +6,11 @@ export const Circle = (props: ComponentPropsWithoutRef<'div'>) => {
   return (
     <div
       className={twMerge(
-        'bg-zinc-900 size-[240px] inline-flex items-center justify-center rounded-full outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10',
+        'bg-zinc-900 size-[240px] inline-flex items-center justify-center rounded-full relative',
         className,
       )}
     >
+      <div className="absolute inset-0 rounded-full outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10 border-[6px] border-transparent border-t-fuchsia-500"></div>
       {children}
     </div>
   );
