26a54b7be429955116ec0179d3bf4377cfcfc8f3
James Kwon <jhk1097@gmail.com>
2025-01-12 20:10:26 +1300

    Using useLayoutEffect to calculate how long the hexagon stroke is

--- src/components/Hexagon.tsx
+++ src/components/Hexagon.tsx
@@ -1,8 +1,14 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import { useLayoutEffect, useRef, type ComponentPropsWithoutRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export const Hexagon = (props: ComponentPropsWithoutRef<'svg'>) => {
   const { className } = props;
+  const pathRef = useRef<SVGPathElement>(null);
+
+  useLayoutEffect(() => {
+    const pathLength = pathRef.current?.getTotalLength();
+  }, []);
+
   return (
     <div className="inline-flex relative">
       <svg
@@ -13,11 +19,12 @@ export const Hexagon = (props: ComponentPropsWithoutRef<'svg'>) => {
         stroke="currentColor"
         strokeWidth="6"
         xmlns="http://www.w3.org/2000/svg"
-        className={twMerge('text-fuchsia-500/10 -rotate-6 absolute', className)}
+        className={twMerge('text-fuchsia-500/30 -rotate-6 absolute', className)}
       >
         <path
           d="M3.44337 38.5C2.55021 36.953 2.55021 35.047 3.44338 33.5L20.0566 4.72501C20.9498 3.178 22.6004 2.22501 24.3868 2.22501H57.6132C59.3996 2.22501 61.0502 3.178 61.9434 4.72501L78.5566 33.5C79.4498 35.047 79.4498 36.953 78.5566 38.5L61.9434 67.275C61.0502 68.822 59.3996 69.775 57.6132 69.775H24.3867C22.6004 69.775 20.9498 68.822 20.0566 67.275L3.44337 38.5Z"
           vector-effect="non-scaling-stroke"
+          ref={pathRef}
         />
       </svg>
       <svg
@@ -29,6 +36,7 @@ export const Hexagon = (props: ComponentPropsWithoutRef<'svg'>) => {
         strokeWidth="6"
         xmlns="http://www.w3.org/2000/svg"
         className={twMerge('text-fuchsia-500/10 -rotate-6', className)}
+        style={{ strokeDasharray: '500 9999', strokeDashoffset: '-2800' }}
       >
         <path
           d="M3.44337 38.5C2.55021 36.953 2.55021 35.047 3.44338 33.5L20.0566 4.72501C20.9498 3.178 22.6004 2.22501 24.3868 2.22501H57.6132C59.3996 2.22501 61.0502 3.178 61.9434 4.72501L78.5566 33.5C79.4498 35.047 79.4498 36.953 78.5566 38.5L61.9434 67.275C61.0502 68.822 59.3996 69.775 57.6132 69.775H24.3867C22.6004 69.775 20.9498 68.822 20.0566 67.275L3.44337 38.5Z"
