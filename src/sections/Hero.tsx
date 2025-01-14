5fe0439bc51755e92ba2b5b0f8909c94b765fb61
James Kwon <jhk1097@gmail.com>
2025-01-14 23:24:48 +1300

    Adding a motion div where our mouse scroll is at

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -111,7 +111,7 @@ export const HeroSection = () => {
         </div>
         <div className="flex justify-center flex-col items-center mt-40 md:mt-80 gap-4">
           <div className="h-10 w-5 outline outline-[6px] outline-fuchsia-500/10 inline-flex justify-center pt-2 rounded-full">
-            <div className="h-3 w-1 bg-fuchsia-500 rounded-full"></div>
+            <motion.div className="h-3 w-1 bg-fuchsia-500 rounded-full" />
           </div>
           <p className="uppercase text-zinc-500 font-extrabold tracking-wider">
             Scroll to learn more
