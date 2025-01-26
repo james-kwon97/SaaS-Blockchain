cf55befa838c42031a242c30b5e772fdbf3f2716
James Kwon <jhk1097@gmail.com>
2025-01-26 17:13:27 +1300

    Creating navLinks dataset for our links to be made

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,27 +1,45 @@
 import { CutCornerButton } from '../components/CutCornerButton';
 
+const navLinks = [
+  { title: 'Home', href: '/' },
+  { title: 'Blog', href: '/blog' },
+  { title: 'Careers', href: '/careers' },
+  { title: 'Contact', href: '/contact' },
+];
+
 export const HeaderSection = () => {
   return (
-    <header className="sticky top-0 z-40 bg-zinc-900/50 backdrop-blur-lg">
-      <div className="container">
-        <div className="flex justify-between items-center h-24 md:h-28">
-          <div>
-            <img src="/assets/images/logo.svg" alt="Blockforge logo" />
-          </div>
-          <div className="flex gap-4 items-center">
-            <CutCornerButton className="hidden md:inline-flex">Get Started</CutCornerButton>
+    <>
+      <header className="sticky top-0 z-40 bg-zinc-900/50 backdrop-blur-lg">
+        <div className="container">
+          <div className="flex justify-between items-center h-24 md:h-28">
+            <div>
+              <img src="/assets/images/logo.svg" alt="Blockforge logo" />
+            </div>
+            <div className="flex gap-4 items-center">
+              <CutCornerButton className="hidden md:inline-flex">Get Started</CutCornerButton>
 
-            <div className="size-10 relative">
-              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
-              </div>
-              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
+              <div className="size-10 relative">
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                  <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
+                </div>
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                  <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </header>
+      <div>
+        <div>
+          <div>
+            <nav>
+              <a href="#">Link</a>
+            </nav>
+          </div>
+        </div>
       </div>
-    </header>
+    </>
   );
 };
