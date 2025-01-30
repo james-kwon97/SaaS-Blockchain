95f7d03a8f14ae65d523a4cf93a9edeb6bc61331
James Kwon <jhk1097@gmail.com>
2025-01-30 22:42:37 +1300

    Increasing margin top size and font sizes in the medium view

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -63,7 +63,7 @@ export const HeaderSection = () => {
             transition={{ duration: 0.5 }}
             className="fixed size-full top-0 left-0 z-30 bg-zinc-900"
           >
-            <div className="absolute inset-2 rounded-md bg-zinc-800 mt-24 z-0">
+            <div className="absolute inset-2 rounded-md bg-zinc-800 mt-24 md:mt-28 z-0">
               <div className="absolute top-1/2 left-full -translate-y-1/2 -translate-x-1/2 -z-10">
                 <Hexagon size={700} />
               </div>
@@ -80,7 +80,7 @@ export const HeaderSection = () => {
                       href={href}
                       key={title}
                     >
-                      <span className="text-4xl font-heading font-black text-zinc-500 hover:text-zinc-300 transition duration-300">
+                      <span className="text-4xl md:text-5xl font-heading font-black text-zinc-500 hover:text-zinc-300 transition duration-300">
                         {title}
                       </span>
                     </motion.a>
